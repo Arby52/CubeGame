0b28575fc61f7c7e1fdf0e3ffdec7dad180c3dc2
Ben Ritson <benjamin.ritson@gmail.com>
2020-10-13 21:12:14 +0100

    Player Movement
    Player movement and jumping implemented. Diagonal movement is faster cus there isn't normalisation on it yet.

---
+++ CubeGame/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    CharacterController controller;
+    Vector3 movement;
+    bool isGrounded;
+    public float speed;
+    public float jumpHeight;
+    public float gravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        movement = Vector3.zero;
+        isGrounded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+        movement.x = xAxis * speed;
+        movement.z = zAxis * speed;
+
+        if (controller.isGrounded && Input.GetButton("Jump"))
+        {
+            movement.y = jumpHeight;
+        }        
+
+        movement.y -= gravity * Time.deltaTime;
+
+        controller.Move(movement *Time.fixedDeltaTime);
+    }
+
+}
