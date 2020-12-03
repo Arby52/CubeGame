8db268cce87b54a0f60c0a5cf4e28ff3805b0365
Ben Ritson <benjamin.ritson@gmail.com>
2020-12-03 13:48:31 +0000

    Pressured Doors Added

---
+++ CubeGame/Assets/Scripts/DoorControlls.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class DoorControlls : MonoBehaviour
+{
+
+    public GameObject linkedDoor;
+    Animator animator;
+
+    public bool onlyWorksWhileBig;
+    public float doorCloseSpeed;
+
+    public Material doorMat;
+    public Material heavyDoorMat;
+
+    private void Start()
+    {
+        try
+        {
+            animator = linkedDoor.GetComponent<Animator>();
+            animator.SetFloat("DoorCloseSpeed", doorCloseSpeed);
+        } catch (System.Exception)
+        {
+            Debug.LogError("Make sure 'Door(drag this one onto the variable)' is in the Linked Door variable in the door controlls script on DoorPad)");
+        }
+    }
+
+    private void Update()
+    {
+        if (onlyWorksWhileBig)
+        {
+            GetComponent<MeshRenderer>().material = heavyDoorMat;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material = doorMat;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {        
+        if (other.gameObject.tag == "Player")
+        {
+            if (onlyWorksWhileBig)
+            {
+                if (other.GetComponent<CharacterMechanics>().cubeSize == CharacterMechanics.SizeStates.big)
+                {
+                    animator.SetBool("Open", true);
+                    animator.SetBool("Close", false);
+                }
+            } else
+            {
+                animator.SetBool("Open", true);
+                animator.SetBool("Close", false);
+            }
+        }        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetBool("Open", false);
+            animator.SetBool("Close", true);
+        }
+    }
+}
