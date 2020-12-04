8c18779926304745f40192e23440b0470de0df9e
Ben Ritson <benjamin.ritson@gmail.com>
2020-12-04 22:32:28 +0000

    Camera gets closer to not intersect with walls

--- CubeGame/Assets/Scripts/CameraController.cs
+++ CubeGame/Assets/Scripts/CameraController.cs
@@ -13,9 +13,17 @@ public class CameraController : MonoBehaviour
 
     float mouseX;
     float mouseY;
-    public Transform cameraHeightRotation;
+    public Transform cameraYMovement;
     public Transform player;
+    public Transform originalPos;
     CharacterMechanics mechanics;
+    Camera cam;
+    float baseDist;
+
+    Vector3 giz1;
+    Vector3 giz2;
+    Vector3 giz3;
+
 
     private void Start()
     {
@@ -24,12 +32,16 @@ public class CameraController : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         mechanics = player.gameObject.GetComponent<CharacterMechanics>();
+        cam = GetComponent<Camera>();
+        Vector3 baseOffset = cam.transform.position - cameraYMovement.position;
+        baseDist = baseOffset.magnitude;
     }
 
     void LateUpdate()
     {
         if (!mechanics.isPaused)
-        {
+        {           
+
             //Camera Stuff
             mouseX += Input.GetAxis("Mouse X") * xSensitivity;
 
@@ -40,10 +52,31 @@ public class CameraController : MonoBehaviour
 
             mouseY = Mathf.Clamp(mouseY, minYAngle, maxYAngle);
 
-            transform.LookAt(cameraHeightRotation);
+            transform.LookAt(cameraYMovement);
 
-            cameraHeightRotation.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            cameraYMovement.rotation = Quaternion.Euler(mouseY, mouseX, 0);
             player.rotation = Quaternion.Euler(0, mouseX, 0);
+
+            //Move Camera Closer if it would intersect with a wall.
+            Vector3 playerToCam = cam.transform.position - cameraYMovement.position;
+            Vector3 behindCam = cam.transform.position + playerToCam.normalized;
+
+            if (Physics.Linecast(cameraYMovement.position, cam.transform.position, LayerMask.NameToLayer("Player")))
+            {
+                if (playerToCam.magnitude >= 0.1)
+                {
+                    cam.transform.position = Vector3.MoveTowards(cam.transform.position, cameraYMovement.transform.position, 0.2f);
+                }
+            }
+            else if (!(Physics.Linecast(cam.transform.position, behindCam, LayerMask.NameToLayer("Player"))))
+            {
+                if (playerToCam.magnitude <= baseDist)
+                {
+                    cam.transform.position = Vector3.MoveTowards(cam.transform.position, originalPos.position, 0.1f);
+                }
+            }
+
         }
     }
+
 }
