e24673ec59e01db101a4b7e4e43526d3cc4df595
Ben Ritson <benjamin.ritson@gmail.com>
2020-12-08 22:45:38 +0000

    cleaned up camera script a bit

--- CubeGame/Assets/Scripts/CameraController.cs
+++ CubeGame/Assets/Scripts/CameraController.cs
@@ -20,11 +20,6 @@ public class CameraController : MonoBehaviour
     Camera cam;
     float baseDist;
 
-    Vector3 giz1;
-    Vector3 giz2;
-    Vector3 giz3;
-
-
     private void Start()
     {
         mouseX = 0;
@@ -40,9 +35,8 @@ public class CameraController : MonoBehaviour
     void LateUpdate()
     {
         if (!mechanics.isPaused)
-        {           
-
-            //Camera Stuff
+        {         
+            //Rotating the camera with the axis inputs
             mouseX += Input.GetAxis("Mouse X") * xSensitivity;
 
             if (invertYAxis)
@@ -61,16 +55,15 @@ public class CameraController : MonoBehaviour
             Vector3 playerToCam = cam.transform.position - cameraYMovement.position;
             Vector3 behindCam = cam.transform.position + playerToCam.normalized;
 
-            RaycastHit hit;
-
-            if (Physics.Linecast(cameraYMovement.position, cam.transform.position, out hit, LayerMask.NameToLayer("Player")))
+            //Move Camera towards the player instantly the distance it needs to not be inside a wall
+            if (Physics.Linecast(cameraYMovement.position, cam.transform.position, out RaycastHit hit, LayerMask.NameToLayer("Player")))
             {
                 if (playerToCam.magnitude >= 0.1)
                 {
                     float toMoveBy = Vector3.Distance(hit.point, cam.transform.position);
                     cam.transform.position = Vector3.MoveTowards(cam.transform.position, cameraYMovement.transform.position, toMoveBy);
                 }
-            }
+            } //If the camera isn't behind an object, and there isnt anything 1 unit behind the camera, smoothly move back by 0.1 units every frame.            
             else if (!(Physics.Linecast(cam.transform.position, behindCam, LayerMask.NameToLayer("Player"))))
             {
                 if (playerToCam.magnitude <= baseDist)
